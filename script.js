e5e2b44977084ad13e68cc5011a7d2f29e3619ec
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-12-14 16:59:56 -0500

    made berry function berry better

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("I'm commiting this first!");
 
-function whichBerry(berry) {
-    console.log(`You just ate a ${berry.type}!`);
+function berryChoice(berry) {
+    console.log(`You choose a ${berry.type}!`);
 }
